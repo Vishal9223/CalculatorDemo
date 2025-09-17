fb6e42cbc22a596c29224f02783c008ffd3ee6f1
Vishal Singh <137828221+Vishal9223@users.noreply.github.com>
2025-09-17 19:31:55 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+let buttons = document.querySelectorAll("button");
+let input=document.querySelector("input");
+for (let button of buttons) {
+  button.addEventListener("click", function (e) {
+    let btnText = e.target.innerText;
+    if (btnText === "AC") {
+      input.value = "";
+    } else if (btnText === "=") {
+      try {
+        input.value = eval(input.value);
+      } catch (err) {
+        input.value = "Gian ho aap";
+      }
+    } else {
+      input.value += btnText;
+    }
+  });
+}
